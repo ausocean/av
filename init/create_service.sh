1721a12096952caf87b1b347dfad9e71e7f800dd
scruzin <anoble@gmail.com>
2024-06-27 16:59:32 +0930

    Install binaries instead of building from source code.

--- init/create_service.sh
+++ init/create_service.sh
@@ -8,24 +8,17 @@ if [ $# -ne 2 ]; then
   exit 1
 fi
 
-# This corresponds to the path beyond the GOPATH corresponding to the run script
-# of the client. e.g. "/src/github.com/ausocean/av/init/run.sh"
-run_script_dir=$1
+# Path to run script,e.g. /opt/ausocean/bin/run.sh
+run_script_path=$1
 
-# This corresponds to the binary dir. e.g. /src/github.com/ausocean/av/cmd/rv
-bin_dir=$2
+# Path to the binary, e.g. /opt/ausocean/bin/rv
+bin_path=$2
 
-# Get the bin name (assuming this is at the end of the bin_dir).
-bin_name=$(basename $bin_dir)
+# We always run as the pi user.
+user=pi
 
-# First find the user that corresponds to this path (which is assumed to be at the
-# base of the current working directory).
-in=$(pwd)
-arr_in=(${in//// })
-gopath_user=${arr_in[1]}
-
-# We can now form the gopath from the obtained user.
-gopath="/home/$gopath_user/go"
+# Get the bin name (assuming this is at the end of the bin_path).
+bin_name=$(basename $bin_path)
 
 # Here are the lines that will go into the rv.service file. We'll set the
 # ExecStart field as the GOPATH we've obtained + the passed run script dir.
@@ -35,7 +28,7 @@ Description=Netsender Client for Media Collection and Forwarding
 
 [Service]
 Type=simple
-ExecStart=$gopath$run_script_dir $gopath_user $bin_dir
+ExecStart=$run_script_path $user $bin_path
 Restart=on-failure
 
 [Install]
